746d3892b0827af2d6983213cdd7f44b9231dac3
cs17b008 <46598027+cs17b008@users.noreply.github.com>
2019-04-20 12:11:44 +0530

    Add files via upload

---
+++ q.s
@@ -0,0 +1,29 @@
+main:
+add $t1,$t2,$t3
+addu $t1,$t1,$t3
+sub $t1,$t1,$t3
+subu $t1,$t1,$t3
+or $t1,$t1,$t3
+and $t1,$t1,$t3
+sllv $t1,$t1,$t3
+slt $t1,$t1,$t3
+srlv $t1,$t1,$t3
+nor $t1,$t1,$t3
+xor $t1,$t1,$t3
+mult $s1,$s2
+multu $s1,$s2
+div $s1,$s2
+divu $s1,$s2
+mfhi $s3
+mflo $s3
+lw $t2,8($s1)
+lb $t2,8($s1)
+sw $t2,8($s1)
+sb $t2,8($s1)
+lui $t3,8
+j loop1
+loop:
+beq $t1,$t2,Exit
+bne $t1,$t2,Exit
+Exit:
+jr $ra
